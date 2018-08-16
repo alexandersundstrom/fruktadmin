62c66f711d191c9c7e098f67964041d2444e7845
Alexander Sundström <alexander.sundstrom@dynabyte.se>
2018-08-16 12:48:25 +0200

    Adds dependency to fruktkort-service

--- src/com/evry/fruktadmin/server/fruktadminServiceImpl.java
+++ src/com/evry/fruktadmin/server/fruktadminServiceImpl.java
@@ -1,11 +1,12 @@
 package com.evry.fruktadmin.server;
-
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.evry.fruktadmin.client.fruktadminService;
 
 public class fruktadminServiceImpl extends RemoteServiceServlet implements fruktadminService {
     // Implementation of sample interface method
-    public String getMessage(String msg) {
+    public String getMessage(String msg)
+    {
+
         return "Client said: \"" + msg + "\"<br>Server answered: \"Hi!\"";
     }
 }
