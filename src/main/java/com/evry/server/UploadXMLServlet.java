fe363a6fffea3c589ed39423260be5e6b5e91117
Alexander Sundström <alexander.sundstrom@dynabyte.se>
2018-08-20 16:16:45 +0200

    Using current schema to parse XML file uploaded

--- src/main/java/com/evry/server/UploadXMLServlet.java
+++ src/main/java/com/evry/server/UploadXMLServlet.java
@@ -1,5 +1,7 @@
 package com.evry.server;
 
+import com.evry.fruktkorgservice.model.ImmutableFruktkorg;
+import com.evry.fruktkorgservice.service.ReportService;
 import org.apache.commons.fileupload.FileItemIterator;
 import org.apache.commons.fileupload.FileItemStream;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
@@ -9,24 +11,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.InputStream;
+import java.util.List;
 
 public class UploadXMLServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) {
         ServletFileUpload upload = new ServletFileUpload();
-
+        ReportService reportService = (ReportService) Beans.getBean("reportService");
         try {
             FileItemIterator iter = upload.getItemIterator(request);
 
             while (iter.hasNext()) {
                 FileItemStream item = iter.next();
 
-                String name = item.getFieldName();
-
                 if (!item.getContentType().equals("text/xml")) {
                     throw new RuntimeException("File must be of typ XML");
                 }
                 InputStream stream = item.openStream();
                 byte[] buffer = IOUtils.toByteArray(stream);
+                List<ImmutableFruktkorg> immutableFruktkorgs = reportService.readFromByteArrayAndUpdateFruktkorgar(buffer);
                 stream.close();
             }
         } catch (Exception e) {
