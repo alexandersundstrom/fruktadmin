7ce088e324776825c5eab8f1e4c4548e6ad61875
Mio Engel <mioengel@gmail.com>
2018-08-21 13:08:55 +0200

    Changes which service is used for file uploading

--- src/main/java/com/evry/server/UploadXMLServlet.java
+++ src/main/java/com/evry/server/UploadXMLServlet.java
@@ -1,36 +1,38 @@
 package com.evry.server;
 
-import com.evry.fruktkorgservice.service.ReportService;
+import com.evry.fruktkorgservice.service.FruktkorgService;
 import org.apache.commons.fileupload.FileItemIterator;
 import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import org.apache.commons.io.IOUtils;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class UploadXMLServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) {
         ServletFileUpload upload = new ServletFileUpload();
-        ReportService reportService = (ReportService) Beans.getBean("reportService");
+        FruktkorgService fruktkorgService = (FruktkorgService) Beans.getBean("fruktkorgService");
         try {
-            FileItemIterator iter = upload.getItemIterator(request);
+            FileItemIterator itr = upload.getItemIterator(request);
+            while (itr.hasNext()) {
+                FileItemStream item = itr.next();
 
-            while (iter.hasNext()) {
-                FileItemStream item = iter.next();
-
-                if (!item.getContentType().equals("text/xml")) {
+                if (!item.getContentType().equals("text/xml") || !item.getContentType().equals("application/xml")) {
                     throw new RuntimeException("File must be of typ XML");
                 }
+
                 InputStream stream = item.openStream();
-                byte[] buffer = IOUtils.toByteArray(stream);
-                reportService.readFromByteArrayAndUpdateFruktkorgar(buffer);
+                fruktkorgService.updateFruktkorgar(stream);
                 stream.close();
             }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (FileUploadException e) {
+            // do something
+        } catch (IOException e) {
+            // do something else
         }
 
     }
