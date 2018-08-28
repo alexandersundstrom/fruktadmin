9048cfc27d6769850d3c2ed7159d1a6694cb8b5c
Mio Engel <mioengel@gmail.com>
2018-08-28 09:49:46 +0200

    Moves button logic to diablog box instead of having it in the UI

--- src/main/java/com/evry/client/model/FruktDialogBox.java
+++ src/main/java/com/evry/client/model/FruktDialogBox.java
@@ -2,26 +2,51 @@ package com.evry.client.model;
 
 import com.evry.client.widget.FruktDialogBoxWidget;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 
 public class FruktDialogBox extends DialogBox {
+    private FruktDialogBoxWidget fruktDialogBoxWidget;
 
-    FruktDialogBoxWidget fruktDialogBoxWidget;
+    private Button okButton;
+    private Button cancelButton;
+
+    private HandlerRegistration okButtonRegistration;
+    private HandlerRegistration cancelButtonRegistration;
+
     public FruktDialogBox(String title, String description) {
 
         fruktDialogBoxWidget = new FruktDialogBoxWidget(title, description);
         setWidget(fruktDialogBoxWidget);
 
+        okButton = fruktDialogBoxWidget.getOkButton();
+        cancelButton = fruktDialogBoxWidget.getCancelButton();
+
         setPopupPosition(400, 100);
     }
 
     public void setOkClickHandler(ClickHandler clickHandler) {
-       fruktDialogBoxWidget.setOkClickHandler(clickHandler, this);
+        if(okButtonRegistration != null) {
+            okButtonRegistration.removeHandler();
+            okButtonRegistration = null;
+        }
+
+        okButtonRegistration = okButton.addClickHandler(event -> {
+            clickHandler.onClick(event);
             hide();
+        });
     }
 
     public void setCancelClickHandler(ClickHandler clickHandler) {
-        fruktDialogBoxWidget.setCancelClickHandler(clickHandler, this);
-        hide();
+        if(cancelButtonRegistration != null) {
+            cancelButtonRegistration.removeHandler();
+            cancelButtonRegistration = null;
+        }
+
+        cancelButtonRegistration = cancelButton.addClickHandler(event -> {
+            clickHandler.onClick(event);
+            hide();
+        });
     }
 }
