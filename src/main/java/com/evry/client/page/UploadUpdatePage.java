bb358a21f4c25bfbc4130481a8387626419f1284
Alexander Sundström <alexander.sundstrom@dynabyte.se>
2018-08-30 10:04:06 +0200

    Forms for Restore and Update XML

--- src/main/java/com/evry/client/page/UploadUpdatePage.java
+++ src/main/java/com/evry/client/page/UploadUpdatePage.java
@@ -1,17 +1,15 @@
 package com.evry.client.page;
 
 import com.evry.client.activity.FruktActivity;
-import com.evry.client.widget.GlassWidget;
-import com.evry.client.widget.MainContentWidget;
+import com.evry.client.model.FruktDialogBox;
+import com.evry.client.widget.*;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.FileUpload;
-import com.google.gwt.user.client.ui.FormPanel;
-import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.*;
 
 public class UploadUpdatePage extends Composite {
     interface MyUiBinder extends UiBinder<Widget, UploadUpdatePage> {}
@@ -21,31 +19,63 @@ public class UploadUpdatePage extends Composite {
     MainContentWidget mainContent;
 
     @UiField
-    FormPanel form;
+    ListBox uploadOptions;
+
+    private FormWidget currentForm;
 
     @UiField
-    FileUpload fileUploader;
-
-    @UiField
-    GlassWidget glass;
+    SimplePanel formContent;
 
     public UploadUpdatePage() {
         initWidget(uiBinder.createAndBindUi(this));
 
         mainContent.setText("Här kan du ladda upp XML filer för att uppdatera fruktkorgar.");
-        fileUploader.getElement().setAttribute("accept", ".xml");
+        uploadOptions.getElement().getFirstChildElement().setAttribute("disabled", "disabled");
+        currentForm = null;
     }
 
-    @UiHandler("form")
-    public void onSubmitForm(FormPanel.SubmitEvent event) {
-        glass.on("Laddar up...");
+    @UiHandler("uploadOptions")
+    public void onChangeFeedbackOptions(ChangeEvent event) {
+        if(currentForm == null) {
+            updateForm();
+            return;
+        }
+
+        if (currentForm.hasUnsubmittedChanges()) {
+            FruktDialogBox dialogBox = new FruktDialogBox("Bekräfta", "Filen har inte skickats, vill du fortsätta?");
+            dialogBox.show();
+
+            if (currentForm instanceof UpdateFormWidget) {
+                dialogBox.setCancelClickHandler(cancelEvent -> uploadOptions.setItemSelected(1, true));
+            } else {
+                dialogBox.setCancelClickHandler(cancelEvent -> uploadOptions.setItemSelected(2, true));
+            }
+
+            dialogBox.setOkClickHandler(okEvent -> {
+                removeForm();
+                updateForm();
+            });
+        } else {
+            removeForm();
+            updateForm();
+        }
     }
 
-    @UiHandler("form")
-    public void onSubmitCompleteFrom(FormPanel.SubmitCompleteEvent event) {
-        glass.off();
+    private void updateForm() {
+        if (uploadOptions.getSelectedValue().equals("update")) {
+            currentForm = new UpdateFormWidget();
+            formContent.add((Widget) currentForm);
+        } else if (uploadOptions.getSelectedValue().equals("restore")) {
+            currentForm = new RestoreFormWidget();
+            formContent.add((Widget) currentForm);
+        }
+    }
 
-        Window.alert(event.getResults());
+    private void removeForm() {
+        if(currentForm != null) {
+            formContent.remove((Widget) currentForm);
+            currentForm = null;
+        }
     }
 
     public void setFruktActivity(FruktActivity fruktActivity) {
---
+++ src/main/java/com/evry/client/widget/UpdateFormWidget.java
@@ -0,0 +1,44 @@
+package com.evry.client.widget;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FileUpload;
+import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class UpdateFormWidget extends Composite implements  FormWidget {
+    interface MyUIBinder extends UiBinder<Widget, UpdateFormWidget> { }
+    private static MyUIBinder uiBinder = GWT.create(MyUIBinder.class);
+
+    @UiField
+    GlassWidget glass;
+
+    @UiField
+    FileUpload fileUploader;
+
+    public UpdateFormWidget() {
+        initWidget(uiBinder.createAndBindUi(this));
+        fileUploader.getElement().setAttribute("accept", ".xml");
+    }
+
+    @UiHandler("form")
+    public void onSubmitForm(FormPanel.SubmitEvent event) {
+        glass.on("Laddar up...");
+    }
+
+    @UiHandler("form")
+    public void onSubmitCompleteFrom(FormPanel.SubmitCompleteEvent event) {
+        glass.off();
+
+        Window.alert(event.getResults());
+    }
+
+    @Override
+    public boolean hasUnsubmittedChanges() {
+        return false;
+    }
+}
