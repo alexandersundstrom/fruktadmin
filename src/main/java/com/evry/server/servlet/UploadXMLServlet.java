613f9fccdde265565fa4eff75b7d2ec8a79dedbf
Alexander Sundström <alexander.sundstrom@dynabyte.se>
2018-09-07 11:14:52 +0200

    Updates class package references

--- src/main/java/com/evry/server/servlet/UploadXMLServlet.java
+++ src/main/java/com/evry/server/servlet/UploadXMLServlet.java
@@ -1,9 +1,9 @@
 package com.evry.server.servlet;
 
 import com.evry.client.util.Log;
-import com.evry.fruktkorg.domain.model.handling.FruktMissingException;
-import com.evry.fruktkorg.domain.model.handling.FruktkorgMissingException;
 import com.evry.fruktkorgservice.FruktkorgService;
+import com.evry.fruktkorgservice.exception.FruktMissingException;
+import com.evry.fruktkorgservice.exception.FruktkorgMissingException;
 import com.evry.server.util.Beans;
 import org.apache.commons.fileupload.FileItemIterator;
 import org.apache.commons.fileupload.FileItemStream;
